d8ff6e80f1c4ac8b3c5d84f9d5f1853c88493d75
Josh Palay <joshpalay@gmail.com>
2013-09-27 17:37:28 -0400

    Added questions 2 and 3

---
+++ assignment1/question2.py
@@ -0,0 +1,35 @@
+class Article:
+    '''
+    Question 2a
+        Properties:
+            - headline
+            - content
+            - creator (author)
+        Methods:
+            - show (print contents)
+            - save (save to text file)
+
+    Question 2b
+        Methods:
+            - Load article from text file
+
+    Question 2d
+        Properties:
+            - related_image
+        Methods:
+            - modify save to save info about related picture (if it exists)
+            - modify load to load info about related picture (if it exists)
+            - modify show to also show the related picture (if it exist)
+    '''
+    pass
+
+class Picture:
+    '''
+    Question 2c
+        Properties:
+            - image_file (path to original image relative to this file)
+            - creator (photographer)
+         Methods
+            - show (show image)
+    '''
+    pass
---
+++ assignment1/question3.py
@@ -0,0 +1,17 @@
+'''
+Question 3 - Just a little feedback for us
+'''
+
+'''
+Question 3a: How long did this assignment take you?
+'''
+time_in_minutes = 0 
+
+'''
+Question 3b: any feedback for us?
+'''
+
+feedback = '''
+    Let us know how things are going so far here.  How are the assignments and
+    presentations so far?  Anything in particular you want us to talk about?
+'''
